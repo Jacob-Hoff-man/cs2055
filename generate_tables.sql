b7fdb9d5b1f21026d53e0ba2920497b1bc5d54b4
Kr-Yan <yan_k1@denison.edu>
2022-11-01 22:20:54 -0400

    V2

--- generate_tables.sql
+++ generate_tables.sql
@@ -2,6 +2,50 @@
 -- CoffeeBoutique CS 1555/2055 Fall 2022
 -- Jacob Hoffman and Kairuo Yan
 ---------------------------------------------
+<<<<<<< Updated upstream
+=======
+DROP TABLE IF EXISTS CUSTOMER CASCADE;
+DROP TABLE IF EXISTS SALE CASCADE;
+
+DROP DOMAIN IF EXISTS Phone_Enum CASCADE;
+DROP DOMAIN IF EXISTS Loyalty_Level_Enum CASCADE;
+
+CREATE DOMAIN Phone_Enum AS varchar(6)
+CHECK (VALUE in ('home', 'mobile', 'work', 'other'));
+
+CREATE DOMAIN Loyalty_Level_Enum AS varchar(10)
+CHECK (VALUE in ('basic', 'bronze', 'silver', 'gold', 'platinum', 'diamond'));
+
+CREATE TABLE CUSTOMER (
+    Customer_Id integer,
+    First_Name varchar(50),
+    Last_Name varchar(50),
+    Mid_Name varchar(50),
+    Birth_Month char(3),
+    Birth_Day char(2),
+    Phone_Number varchar(16),
+    Phone_Type Phone_Enum,
+    Current_Points float,
+    Career_Points float,
+    Loyalty_Level Loyalty_Level_Enum,
+
+    CONSTRAINT PK_CUSTOMER PRIMARY KEY (Customer_Id),
+    CONSTRAINT UQ_PHONE UNIQUE (Phone_Number)
+);
+
+CREATE TABLE SALE (
+  Purchase_Id integer,
+  Purchased_Time time,
+  Purchased_Portion float,
+  Redeemed_Portion float,
+  Customer_Id int NOT NULL,
+  Store_Id int NOT NULL,
+  Coffee_Id int NOT NULL,
+
+  CONSTRAINT PK_SALE PRIMARY KEY (Purchase_Id)
+);
+
+>>>>>>> Stashed changes
 DROP DOMAIN IF EXISTS store_type CASCADE;
 CREATE DOMAIN store_type varchar(7)
 CONSTRAINT store_type_value CHECK(VALUE IN('kiosk', 'sitting store'));
@@ -29,6 +73,29 @@ CREATE TABLE PROMOTION(
     CONSTRAINT UQ_Pname UNIQUE (Promo_Name)
 
 );
+
+DROP TABLE IF EXISTS OFFERS cascade;
+CREATE TABLE OFFERS(
+    Promo_Number int NOT NULL,
+    Coffee_Id int NOT NULL,
+    Store_Number int NOT NULL,
+    CONSTRAINT PK_OFFERS PRIMARY KEY (Promo_Number, Store_Number),
+    CONSTRAINT FK1_OFFERS FOREIGN KEY (Store_Number) REFERENCES STORE (Store_Number)
+                  ON DELETE CASCADE,
+    CONSTRAINT FK2_OFFERS FOREIGN KEY (Coffee_Id) REFERENCES COFFEE (Coffee_Id)
+                  ON DELETE CASCADE
+)
+
+DROP TABLE IF EXISTS FEATURES cascade;
+CREATE TABLE FEATURES(
+    Coffee_Id int NOT NULL,
+    Store_Number int NOT NULL,
+    CONSTRAINT PK_FEATURES PRIMARY KEY (Coffee_Id, Store_Number),
+    CONSTRAINT FK1_FEATURES FOREIGN KEY (Store_Number) REFERENCES STORE (Store_Number)
+                  ON DELETE CASCADE,
+    CONSTRAINT FK2_FEATURES FOREIGN KEY (Coffee_Id) REFERENCES COFFEE (Coffee_Id)
+                  ON DELETE CASCADE
+)
 
 DROP TABLE IF EXISTS COFFEE cascade;
 CREATE TABLE COFFEE(
